498fb613dd239475a3a24729cb76f8f83cfce0e7
Alvaro <alvaroreyes_shadow@hotmail.com>
2025-01-24 15:04:02 +0100

    Update

--- midipart-server.py
+++ midipart-server.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from src.main.controller.analyzer_controller import analyzer_blueprint
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["https://ntok5eh8dg.execute-api.eu-south-2.amazonaws.com", "http://midiloadbalancer-33bc8d136623c566.elb.eu-south-2.amazonaws.com"]}})
+CORS(app, resources={r"/*": {"origins": ["https://web.midirating.com", "https://ntok5eh8dg.execute-api.eu-south-2.amazonaws.com", "http://midiloadbalancer-33bc8d136623c566.elb.eu-south-2.amazonaws.com"]}})
 
 # Register the blueprint for the analyzer controller
 app.register_blueprint(analyzer_blueprint)
