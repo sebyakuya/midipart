1f0884f9f87ec044d9b3873816ef79247eb847ce
Alvaro <alvaroreyes_shadow@hotmail.com>
2025-01-24 11:50:03 +0100

    Update

--- midipart-server.py
+++ midipart-server.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from src.main.controller.analyzer_controller import analyzer_blueprint
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 # Register the blueprint for the analyzer controller
 app.register_blueprint(analyzer_blueprint)
